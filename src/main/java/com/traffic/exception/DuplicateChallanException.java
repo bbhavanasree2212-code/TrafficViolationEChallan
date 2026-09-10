24a625e42200760f677a1887329611773fe59c78
bbhavanasree2212-code <bbhavanasree2212@gmail.com>
2026-09-10 15:44:53 +0530

    Create DuplicateChallanException.java

---
+++ src/main/java/com/traffic/exception/DuplicateChallanException.java
@@ -0,0 +1,8 @@
+package com.traffic.exception;
+
+public class DuplicateChallanException extends Exception {
+
+    public DuplicateChallanException(String message) {
+        super(message);
+    }
+}
